f2177bbf79a0979b38ba5acb5ef13bbacf5ec1ed
NdubuisiJr <ndubuisijrchukuigwe@gmail.com>
2020-08-08 18:20:55 +0100

    started implementing DonutChart

---
+++ SkiaChart/Charts/DonutChart.cs
@@ -0,0 +1,13 @@
+﻿using SkiaChart.Axes;
+using SkiaChart.Interfaces;
+using SkiaChart.Models;
+using System;
+
+namespace SkiaChart.Charts {
+    public class DonutChart : RadialChart, ISingleValueChart {
+        public DonutChart(string label, float value) : base(label,value) {}
+        public override void RenderChart(CanvasWrapper canvas, Axis axis, IMinMax minMax) {
+            throw new NotImplementedException();
+        }
+    }
+}
