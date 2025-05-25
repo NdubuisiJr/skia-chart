9ec565226dc3180b8e9be772cc80677d98572001
ejimeoghenefejiro <ejimeoghenefejiro@yahoo.com>
2025-05-25 23:21:37 +0100

    feat: #25 - Integrated the spline algorithm to AreaChart series

--- SkiaChart/Charts/AreaChart.cs
+++ SkiaChart/Charts/AreaChart.cs
@@ -1,20 +1,24 @@
 ﻿using SkiaChart.Axes;
 using SkiaChart.Enums;
+using SkiaChart.Helpers;
 using SkiaChart.Interfaces;
 using SkiaChart.Models;
 using SkiaSharp;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace SkiaChart.Charts {
-    public class AreaChart : LineChart {
+namespace SkiaChart.Charts
+{
+    public class AreaChart : LineChart
+    {
         /// <summary>
         /// Instantiates an instance of Area with floating point values
         /// </summary>
         /// <param name="xValues">X-Cordinates of the X-Y plot</param>
         /// <param name="yValues">Y-Cordinates of the X-Y plot</param>
         public AreaChart(IEnumerable<float> xValues, IEnumerable<float> yValues)
-            : base(xValues, yValues) {
+            : base(xValues, yValues)
+        {
         }
 
         /// <summary>
@@ -23,7 +27,8 @@ namespace SkiaChart.Charts {
         /// <param name="xValues">X-Cordinates of the X-Y plot as string labels</param>
         /// <param name="yValues">Y-Cordinates of the X-Y plot</param>
         public AreaChart(IEnumerable<string> xValues, IEnumerable<float> yValues)
-             : base(xValues, yValues) {
+             : base(xValues, yValues)
+        {
         }
 
         /// <summary>
@@ -32,7 +37,8 @@ namespace SkiaChart.Charts {
         /// <param name="xValues">X-Cordinates of the X-Y plot</param>
         /// <param name="yValues">Y-Cordinates of the X-Y plot as string labels</param>
         public AreaChart(IEnumerable<float> xValues, IEnumerable<string> yValues)
-             : base(xValues, yValues) {
+             : base(xValues, yValues)
+        {
         }
 
         /// <summary>
@@ -42,33 +48,35 @@ namespace SkiaChart.Charts {
         /// <param name="axis">Axis orientation object</param>
         /// <param name="minMax">Data for the extreme values</param>
         /// <param name="gridPaint">Paint object for the grid lines</param>
-        public override void RenderChart(CanvasWrapper canvasWrapper, Axis axis, IMinMax minMax) {
+        public override void RenderChart(CanvasWrapper canvasWrapper, Axis axis, IMinMax minMax)
+        {
             CheckConstructionPolicy(nameof(LineChart));
 
-            if (canvasWrapper.NumberPlottedChart < 1) {
+            if (canvasWrapper.NumberPlottedChart < 1)
+            {
                 DrawHorizontalLabels(canvasWrapper, axis, minMax);
                 DrawVerticalLabels(canvasWrapper, axis, minMax);
             }
 
             var canvas = canvasWrapper.Canvas;
-            var path = new SKPath();
-            var firstPoint = ConstructionData.First();
-            var lastPoint = ConstructionData.Last();
-            var connectionPoint = new SKPoint(lastPoint.X, firstPoint.Y);
-            ConstructionData.Add(connectionPoint);
-            path.MoveTo(ConstructionData.First());
-            foreach (var point in ConstructionData.Skip(1)) {
-                path.LineTo(point);
-                if(ShowPoints)
+            var path = SplineHelper.CreateCatmullRomSpline(ConstructionData, closed: true);
+
+            if (ShowPoints)
+            {
+                foreach (var point in ConstructionData.Skip(1))
+                {
                     canvas.DrawCircle(point, PointRadius, _chartPaint);
+                }
             }
+
             path.FillType = SKPathFillType.EvenOdd;
             path.Close();
             _chartPaint.IsStroke = false;
             canvas.DrawPath(path, _chartPaint);
             canvasWrapper.NumberPlottedChart += 1;
 
-            if (canvasWrapper.CanShowLegend) {
+            if (canvasWrapper.CanShowLegend)
+            {
                 RenderLegend(canvasWrapper, axis, canvas, PointPlotVariant.AreaChart);
             }
         }
